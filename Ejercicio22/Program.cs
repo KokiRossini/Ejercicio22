2529535c2506d2b341a78f153d3df69ae774b92b
Juan Ignacio Rossini <koki-rossini@hotmail.com.ar>
2021-06-07 15:20:16 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio22/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperatura;
+            Double temperaturaComvertida;
+            
+
+            Console.WriteLine("opciones a convertir");
+            Console.WriteLine("(C) de celcius a farenheit");
+            Console.WriteLine("(F) de Farenheit a celcius");
+
+            var opciones = Console.ReadLine();
+
+            if (opciones.ToUpper() == "C" || (opciones.ToUpper() == "F"))
+            {
+                if (opciones.ToUpper() == "C")
+                {
+                    Console.WriteLine("ingrese temperatura en celcius");
+                    temperatura = Convert.ToInt32(Console.ReadLine());
+                    temperaturaComvertida = 1.8 * temperatura + 32;
+                    Console.WriteLine($" {temperatura} grados celcius son: {temperaturaComvertida} grados farenheit");
+                }
+
+                else
+                {
+                    Console.WriteLine("ingrese temperatura en farenheit");
+                    temperatura = Convert.ToInt32(Console.ReadLine());
+                    temperaturaComvertida = (temperatura - 32) / 1.8;
+                    Console.WriteLine($"{temperatura} grados farenheit son: {temperaturaComvertida} grados celcius");
+                }
+
+            }
+
+            else
+            {
+                Console.WriteLine("opcion ingresada no valida");
+            }
+        }
+    }
+}
